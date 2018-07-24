a68f7b137e944a9bf8d480710a2aaedc702b6a2b
Ben Fairfield <benfa@umich.edu>
2018-07-24 16:38:28 -0400

    new start.sh file

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# Redirect logs to stdout and stderr for docker reasons.
+ln -sf /dev/stdout /var/log/apache2/access_log
+ln -sf /dev/stderr /var/log/apache2/error_log
+
+# for jenkins server
+ln -sf /secrets/ssl/benfa-jenkins.webplatformsnonprod.umich.edu.cert \
+	/etc/ssl/certs/benfa-jenkins.webplatformsnonprod.umich.edu.cert
+
+ln -sf /secrets/ssl/benfa-jenkins.webplatformsnonprod.umich.edu.key \
+	 /etc/ssl/private/benfa-jenkins.webplatformsnonprod.umich.edu.key
+
+# SSL secrets
+ln -sf /secrets/ssl/USERTrustRSACertificationAuthority.pem /etc/ssl/certs/USERTrustRSACertificationAuthority.pem
+ln -sf /secrets/ssl/AddTrustExternalCARoot.pem /etc/ssl/certs/AddTrustExternalCARoot.pem
+ln -sf /secrets/ssl/sha384-Intermediate-cert.pem /etc/ssl/certs/sha384-Intermediate-cert.pem
+
+## Rehash command needs to be run before starting apache.
+c_rehash /etc/ssl/certs /secrets/ssl
+
